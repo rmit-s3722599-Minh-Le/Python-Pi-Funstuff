df82893c09173f0e5dcc052d9e0e3509b6602794
Minh <s3722599@student.rmit.edu.au>
2020-10-26 17:25:16 +1100

    added logical works of words rearranging

--- MuddleWords.py
+++ MuddleWords.py
@@ -1,15 +1,35 @@
+import random
 
 #main functionality
 def main():
     print("hello")
     #the user input of words
-    inputWords = input()
+    sentence = input()
+    rearrangeWords(sentence)
 
+def uniqueShuffle(subStr):
+    newShuffle = ''.join(random.sample(subStr, len(subStr)))
+    if (len(subStr) > 3):
+        while newShuffle == subStr:
+            newShuffle = ''.join(random.sample(subStr, len(subStr)))
+    return newShuffle
+    
 
-def rewrrangeWord(word):
-    alterWord = ""
-    for i in range(1,len(word)):
-        print("unfinished")
+def rearrangeWord(word):
+    if (len(word) != 1):
+        subStr = word[1:len(word) - 1]
+        alterWord = word[0] + uniqueShuffle(subStr) + word[-1]
+        return alterWord
+    else:
+        return word
+    # for i in range(1,len(word)):
+    #     print("unfinished")
+
+def rearrangeWords(sentence):
+    # newSentence = ""
+    wordsList = sentence.split(" ")
+    for word in wordsList:
+        print(rearrangeWord(word))
 
 if __name__ == "__main__":
     main()
