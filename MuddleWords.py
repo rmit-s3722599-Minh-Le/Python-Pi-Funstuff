20fae91fe99a7fd32cc47b49480bde50566c070b
Minh <s3722599@student.rmit.edu.au>
2020-10-26 16:14:06 +1100

    init start code

---
+++ MuddleWords.py
@@ -0,0 +1,15 @@
+
+#main functionality
+def main():
+    print("hello")
+    #the user input of words
+    inputWords = input()
+
+
+def rewrrangeWord(word):
+    alterWord = ""
+    for i in range(1,len(word)):
+        print("unfinished")
+
+if __name__ == "__main__":
+    main()
